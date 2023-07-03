15a0f66eb64807e8a88a9f83b16df1bcedb8d825
Tanvi Jain <iamtanvi11@gmail.com>
2023-07-03 17:45:59 +0530

    Final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
